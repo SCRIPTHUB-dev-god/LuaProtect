24644a8c68080c7fe09e23c3486e7865eff7b3dd
SCRIPTHUB-dev-god <faruqaldaffa069@gmail.com>
2026-09-20 16:52:28 +0700

    Update raw.js

--- api/raw.js
+++ api/raw.js
@@ -8,22 +8,22 @@ export default async function handler(req, res) {
 
     if (data && data.length === 32) {
         try {
-            const kvRes = await fetch(`https://kvdb.io/LuaProtectStore2026/${data}`);
+            const kvRes = await fetch(`https://kvdb.io/LuaProtectBucket2026/${data}`);
             if (kvRes.ok) {
                 const kvData = await kvRes.json();
                 if (kvData.status === 'public') {
                     luaCode = kvData.code;
                 } else {
-                    luaCode = "warn('LuaProtect: Script ini dalam status UNPUBLIC / DISABLED')";
+                    luaCode = "warn('LuaProtect: Script is UNPUBLIC / DISABLED')";
                 }
             } else {
-                luaCode = "warn('LuaProtect: Key 32-digit tidak ditemukan atau kadaluarsa')";
+                luaCode = "warn('LuaProtect: Invalid or expired 32-digit Hex Key')";
             }
         } catch (e) {
-            luaCode = "warn('LuaProtect: Gagal mengambil data dari server database')";
+            luaCode = "warn('LuaProtect: Database connection error')";
         }
     } else {
-        luaCode = "warn('LuaProtect: Parameter data harus berupa 32-digit Hex Key')";
+        luaCode = "warn('LuaProtect: Invalid request parameter (32-digit Hex required)')";
     }
 
     const html = `--[=[\n<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <title>404: This page could not be found</title>\n    <style>\n        * { margin: 0; padding: 0; box-sizing: border-box; }\n        body { background-color: #000000; color: #ffffff; font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif; height: 100vh; display: flex; align-items: center; justify-content: center; font-size: 0; }\n        .error-wrapper { display: flex; align-items: center; justify-content: center; font-size: 14px; }\n        .error-code { font-size: 24px; font-weight: 500; padding-right: 23px; margin-right: 20px; border-right: 1px solid rgba(255, 255, 255, 0.3); line-height: 49px; }\n        .error-message { font-size: 14px; font-weight: 400; line-height: 49px; }\n    </style>\n</head>\n<body>\n    <div class="error-wrapper">\n        <div class="error-code">404</div>\n        <div class="error-message">This page could not be found.</div>\n    </div>\n</body>\n</html>\n]=]\n${luaCode}`;
